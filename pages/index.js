96200ec34965059917b71d12d31c2ee60c2e2570
Jerry Ye <jerry1ye10@gmail.com>
2025-06-16 21:33:39 -0700

    update personal site

--- pages/index.js
+++ pages/index.js
@@ -14,22 +14,27 @@ export default function Home() {
       <div className="flex px-5 sm:px-20 md:px-40 py-5">
         <div class="flex-2 text-auto px-2">
           <p className="py-2">
-          Hi! I'm Jerry, a 23 year old UPenn dropout based in SF. I was born & raised in NYC where I went to Stuyvesant High School. 
+          Hi! I'm Jerry! I'm 23, born & raised in NYC where I went to Stuyvesant High School.
           </p>
 
           <p className="py-2">
-          I dropped out of college after my freshman year to explore what I wanted to do in life. Most recently,  I worked on Archer, an AI call center for home service businesses backed by Bloomberg Beta & 1984 Ventures.  We were acquired by our largest partner, a human based call center named <a href="https://www.free-2-grow.com/" target="_blank" rel="noopener noreferrer" className="text-blue-500">Free2Grow</a>, where I'm now the CTO. 
+          I went to the University of Pennsylvania & dropped out of college after my freshman year to explore what I wanted to do in life. I'm currently the CTO & Co-founder at <a href="https://www.free-2-grow.com/" target="_blank" rel="noopener noreferrer" className="text-blue-500">Free2Grow</a>, where I've scaled the business to $1M+ ARR. I own all things product, client success, and email marketing.
           </p>
 
        
 
           <p className="py-2">
-            In the past, I also built <a href="https://www.founderschoicevc.com/" target="_blank" rel="noopener noreferrer" className="text-blue-500">Founder's Choice</a>, a vc ranking website which aggregated data from 1077 venture back founders. We were sponsored by AngelList, Crunchbase, 2.12 Angels, BBG Ventures, Bloomberg Beta, Precursor ventures, & SignalFire. 
+            In the past, I also built <a href="https://www.founderschoicevc.com/" target="_blank" rel="noopener noreferrer" className="text-blue-500">Founder's Choice</a>, a vc ranking website which aggregated data from 1077 venture back founders and was covered by journalists like <a href="https://www.newcomer.co/p/founders-choice-vc-rankings-revealed" target="_blank" rel="noopener noreferrer" className="text-blue-500">Eric Newcomer</a>. We were sponsored by AngelList, Crunchbase, 2.12 Angels, BBG Ventures, Bloomberg Beta, Precursor ventures, & SignalFire. 
           </p>
 
           <p className="py-2">
-          For fun, I like anime + have always been a huge gamer. I hit challenger (rank 85, top .01%) in Teamfight tactics recently. I also enjoy playing poker and am actively hosting games in SF. Hit me up if you'd like to play some time! 
+            Some things about me I'm proud of:
           </p>
+          <ul className="py-2 list-disc list-inside space-y-1">
+            <li>As a kid, I sold Yu-Gi-Oh! cards for cash. My parents wouldn't let me order online, so I had to ship everything to a friend's house and pay him a "handling fee"—still turned $20 into $10K.</li>
+            <li>I've been top ranked at many games (1200 LP Challenger / Rank 80 NA in TFT, Top 10 DN ranking in yugioh, top 100 backyard monsters player worldwide, Diamond 2 in league of legends, etc)</li>
+            <li>In high school, I broke into NYC's health policy world to stand out for college apps. I hustled my way into speaking at conferences, built relationships from scratch, launched city-wide initiatives, and became the youngest intern at the Department of Education. Unironically, years later, this became the best crash course into how to hustle in business.</li>
+          </ul>
           <p className="py-2">
             These days, I'm in SF actively exploring & looking to meet more people. If you think I'm cool, hit me up and let's grab food or play poker some time! 
           </p>
